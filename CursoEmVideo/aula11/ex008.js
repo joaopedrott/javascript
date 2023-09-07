30f6434db295e5b40382fd4ace0e359d4e2533ce
joaopedrogueds <joaopedroguedestorres@gmail.com>
2023-09-07 01:07:12 -0300

    criei ex008, 009 e ex 010

---
+++ CursoEmVideo/aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 57
+console.log(`A velocidade do seu carro e ${vel} km/h`)
+if (vel > 60) {
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de seguranca!`)

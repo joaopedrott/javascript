a16a9b530d19afab7864cdcabc1324e4dea93239
joaopedrogueds <joaopedroguedestorres@gmail.com>
2023-09-12 21:47:58 -0300

    criei aula 13

---
+++ CursoEmVideo/aula13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+while(c<6){
+    console.log('tudo bem?')
+    c++
+}
+
+
+
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
---
+++ CursoEmVideo/aula13/node_86529ba867098.tmp.js
@@ -0,0 +1,17 @@
+var c = 1
+while(c<6){
+    console.log('tudo bem?')
+}
+
+
+
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/

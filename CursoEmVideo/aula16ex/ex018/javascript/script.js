08f5d053f66f56055e2933e6820d6fd1e407047e
joaopedrogueds <joaopedroguedestorres@gmail.com>
2023-09-20 22:51:06 -0300

    atualizei o desafio

--- CursoEmVideo/aula16ex/ex018/javascript/script.js
+++ CursoEmVideo/aula16ex/ex018/javascript/script.js
@@ -6,17 +6,19 @@ var tab = window.document.getElementById('igrupo')
 
 
 function adicionar(){
-    numero = parseInt(numero.value)
-    if (lista.includes(numero) || numero <1 || numero>100 || Number.isNaN(numero)){
+    //numero = parseInt(numero.value)
+    if (lista.includes(numero.value) || numero.value <1 || numero.value>100 || Number.isNaN(numero.value)){
         window.alert('Numero invalido ou ja encontrado na lista')
 
     } else {
         let item = window.document.createElement('option')
-        lista.push(numero)
-        item.text=`o varlor ${numero} foi adicionado`
-        item.value= `tab${numero}`
+        lista.push(numero.value)
+        item.text=`o varlor ${numero.value} foi adicionado`
+        item.value= `tab${numero.value}`
         tab.appendChild(item)
     }
+    inum.value = ''
+    inum.focus()
 } 
 
 function somando(){
@@ -31,14 +33,14 @@ function finalizado(){
     if(lista.length==0){
         window.alert(`Adicione valores antes de finalizar`)
     } else {
-        
+        res.innerHTML=''
         lista.sort()
-        res.innerHTML =`<br>Ao todo, temos ${lista.length} numeros cadastrados.<br>`
-        res.innerHTML += `<br>O maior valor informado foi ${lista[lista.length-1]}.<br>`
-        res.innerHTML += `<br>O menor valor informado foi ${lista[0]}. <br>`
-        let soma=somando()
-        res.innerHTML += `<br>Somando todos os valores, temos ${soma}.<br>`
-        res.innerHTML +=`<br>A media de todos os valores e ${parseFloat(soma/lista.length)}<br>`
+        res.innerHTML =`<p>Ao todo, temos ${lista.length} numeros cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${lista[lista.length-1]}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${lista[0]}. </p>`
+        //let soma=somando()
+        res.innerHTML += `<p>Somando todos os valores, temos ${somando()}.</p>`
+        res.innerHTML +=`<p>A media de todos os valores e ${parseFloat(somando()/lista.length)}</p>`
     }
     
 

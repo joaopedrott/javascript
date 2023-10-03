abefd7b255ab463ede80d9f7d1c91559c685c322
joaopedrogueds <joaopedroguedestorres@gmail.com>
2023-10-03 12:00:01 -0300

    importacao

---
+++ Dio/Aulas/aula7 - importacao/funcoes-auxiliares.js
@@ -0,0 +1,15 @@
+
+
+function gets(){
+    return 10;
+}
+
+function print(texto){
+        console.log(texto);
+}
+
+
+module.exports = {
+    gets,
+    print
+};
---
+++ Dio/Aulas/aula7 - importacao/main.js
@@ -0,0 +1,11 @@
+
+/* 
+const funcoes = require('./funcoes-auxiliares');
+
+console.log(funcoes.gets());
+ */
+
+//com object destruturing
+const {gets,print} = require('./funcoes-auxiliares')
+
+print(gets())

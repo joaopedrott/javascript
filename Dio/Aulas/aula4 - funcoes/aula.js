ebd70d2d20e95ec61ed79413005c2a800acc274a
joaopedrogueds <joaopedroguedestorres@gmail.com>
2023-10-04 18:33:22 -0300

    atualizei aula 4 e criei aula8

--- Dio/Aulas/aula4 - funcoes/aula.js
+++ Dio/Aulas/aula4 - funcoes/aula.js
@@ -1,4 +1,34 @@
-function teste(){
+function teste() {
     console.log(`teste`)
 }
 
+
+//definindo a funcao principal
+function main() {
+    console.log('teste222');
+}
+
+//atualizando a funcao principal
+main = function () {
+    console.log(2)
+}
+
+main();//o resultado vai ser 2 pois a funcao main foi atualizada com console.log(2)
+
+
+/*  Para uma funcao principal mais dinamica, podemos fazer assim:
+1 - funcao normal
+function main() {
+    console.log(1);
+}
+main();
+
+2 - funcao imediata ou funcao imediatamente invocada
+    (function main() {
+        console.log(1);
+    })();
+
+3 - funcao imediatamente invocada nao nomeada //neste caso nao podemos atualizar pois nao temos o nome pq esta nao nomeada
+    (function () {
+        console.log(1);
+    })();   */
---
+++ Dio/Aulas/aula8 - conjunto de exercicios/desafio-NotaAluno.js
@@ -0,0 +1,19 @@
+//funcoes adicionais eu coloco aqui fora
+
+(function () {
+
+    const { gets, print } = require('./funcoes-auxiliares')
+    const nota = gets();
+
+    print(nota);
+
+    if (nota < 5) {
+        print('Reprovado!')
+    } else if (nota >= 5 && nota < 7) {
+        print('Recuperacao')
+    } else { (nota >= 7)
+        print('Aprovado')
+    }
+
+
+})();

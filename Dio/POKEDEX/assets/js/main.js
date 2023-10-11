844e7a4f31b1b38d8bc6791733964552c978c7c6
joaopedrogueds <joaopedroguedestorres@gmail.com>
2023-10-11 14:29:28 -0300

    comecei a fazer a requisicao dos pokemons

--- Dio/POKEDEX/assets/js/main.js
+++ Dio/POKEDEX/assets/js/main.js
@@ -1,2 +1,11 @@
 
-console.log('sucesso!');
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url).then(function (response){//metodo then processa o sucesso de uma promise(promessa de resposta da requisicao)
+    console.log(response)
+}) // processamento assincrono
+
+const x = 10+10
+console.log(x)

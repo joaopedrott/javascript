2a0dadc9e959e8b059d232d9479ac17d8f77fafc
joaopedrogueds <joaopedroguedestorres@gmail.com>
2023-10-25 15:11:49 -0300

    atualizando aula 10

---
+++ Dio/Aulas/aula11 - tiposDeDados/aula.js
@@ -0,0 +1,45 @@
+
+/* tipos primitivos de dados
+    boolean
+    null
+    undefined
+    number
+    sting 
+    synbol
+
+    //obs: tipos primitivos sao imutaveis
+*/
+
+//object
+//obs: objetos tem metodos
+//-----------------------------------
+//boolean
+//Wrappers para encapsular
+// existem classes ou wrappers que representam os tipos primitivos para mudar valor da variavel 
+// true, false
+//new Boolean(true)
+//-----------------------------------
+//coercao de tipos
+//eh a convercao de tipos como:
+// (10 + "10") = 1010
+//------------------------------------
+//NULL
+// a variavel existe mas nao tem valor dentro dela
+//------------------------------------
+//undefined
+//inexistencia da variavel
+//ex: usando objeto
+/* 
+    const x ={nome: null};
+    console.log(x.nome); // resultado eh null
+    console.log(x.idade); // undefined pois nao existe a variavel idade
+*/
+
+/*
+const a; // vai dar erro pois necessita iniciar a variavel com um valor
+let a; //undefined pois eh considerada ausencia de declaracao
+let a=null//null ausencia de valor
+
+
+*/
+//------------------------------------

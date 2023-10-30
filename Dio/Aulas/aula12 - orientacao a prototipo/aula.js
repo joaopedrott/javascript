5e511790c1b04e99ca3c412db9520cf419ed6d17
joaopedrogueds <joaopedroguedestorres@gmail.com>
2023-10-30 13:36:29 -0300

    aula 12 sobre classes

---
+++ Dio/Aulas/aula12 - orientacao a prototipo/aula.js
@@ -0,0 +1,51 @@
+// orientacao a prototipo, funcoes construtoras
+//orientacao a prototipo semelhante a classes, semelhando ao polimorfismo e etc
+//-------------Exemplo-------------------
+//heranca entre objetos
+
+/* const pessoa = {
+    genero: 'masculino'
+}
+
+const renan = {//objeto renan herda de pessoa o genero
+    nome: 'renan',
+    idade:30,
+    __proto__: pessoa//ligacao de heranca
+}
+
+console.log(renan.genero)
+ */
+//------------------------------------------------------------
+//classes no fim sao funcoes construtoras no javascript
+//exemplo de funcao construtora
+
+ // isso eh uma classe ou funcao construtora em javascript
+function Pessoa(nome,idade){
+    this.nome = nome
+    this.idade = idade
+
+}
+
+//adicionando metodo a "classe" Pessoa
+//para adicionar vamos mudar o prototipo da Pessoa!
+ Pessoa.prototype.falar= function(){//funcao anonima ou sem nome
+    console.log(`Meu nome e: ${this.nome}`)
+}  
+
+
+
+//---------------------------------------------------
+//Exemplo de como eh a criacao de uma classe normalmente
+//AMBOS SAO EQUIVALENTES, FUNCAO PESSOA OU CLASSE PESSOA
+/* class Pessoa {
+    constructor (nome,idade){
+        this.nome = nome,
+        this.idade = idade
+    }
+    falar(){
+        console.log(`Meu nome e: ${this.nome}`)
+    }
+} */
+const renan = new Pessoa('renan', 30)
+console.log(renan)
+renan.falar()

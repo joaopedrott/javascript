2d4d1623f04383cfb85077e17b18cb80eeeb13c5
joaopedrogueds <joaopedroguedestorres@gmail.com>
2023-11-01 13:19:48 -0300

    fiz alteracoes nas anotacoes sobre as funcoes

--- Dio/Aulas/aula13 - funcoes e manipulacao de listas/aula1 .js
+++ Dio/Aulas/aula13 - funcoes e manipulacao de listas/aula1 .js
@@ -2,15 +2,15 @@
 
 //As funcoes do javascript sao:
 //First Class Functions
-//ou
+//e
 //Higher Order Functions
 //----------------Exemplo-----------------
-/* function falarMeuNome(){
+ function falarMeuNome(){
     console.log('Meu nome e Renan')
-} */
+} 
 
 /* 
-//ex 1 referencia de funcao
+//ex 1 referencia de funcao ou passando funcao para uma variavel
 const referenciaNova = falarMeuNome
 
 referenciaNova()
@@ -28,21 +28,36 @@ falarMeuNomeCompleto(falarMeuNome)
 
  */
 //----------------Exemplo-----------------
+ 
+//ex 2.1 passando funcao como parametro para outra funcao com RETORNO DE FUNCAO e invocando ela
+//uma funcao dentro de outra, 
+function falarMeuNomeCompleto(falarMeuNome) {
+    falarMeuNome()
+    console.log('Johannsen de Paula')
+    return falarMeuNome
+}
 
+falarMeuNomeCompleto(falarMeuNome)()//o parentese adicional vazio eh para executar a funcao retornada
+
+ 
 //----------------------------------------------------
 //duas caracteristicas para declarar uma funcao
 //1 function expression
 //2 function declaration
 
 //2 function declaration
-function nomeDaFuncao() {
+/* function nomeDaFuncao() {
     console.log('nomeDaFuncao')
 }
-
+ */
 //1 function expression
+/* 
 const nomeDeOutraFuncao = function () {
     console.log('nomeDeOutraFuncao')
 }
+ */
+/* 
 nomeDaFuncao()
 nomeDeOutraFuncao()
+ */
 //obs: a grande diferenca eh o hoisting, que eh a situacao de quando o codigo eh executado. A function declaration sofre o hoisting indo para o topo do codigo mas a function expression somente a declaracao da constante eh jogada pra cima enquanto a atribuicao da funcao a essa constante continua no mesmo lugar.

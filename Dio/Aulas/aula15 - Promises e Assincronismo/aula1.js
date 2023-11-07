9afd6af354d4520b33b31c3ad3072e24986e76eb
joaopedrogueds <joaopedroguedestorres@gmail.com>
2023-11-07 11:23:11 -0300

    Aula 15 promises

---
+++ Dio/Aulas/aula15 - Promises e Assincronismo/aula1.js
@@ -0,0 +1,48 @@
+//Funcionamento das promises
+//Assincronismo eh quando uma coisa nao executa necessariamente imediatamente(demora a executar)
+//temos vantagem em usar promises pois tem bastante coisas traduzidas para elas
+//criando uma promessa
+
+/* const promessaDeUmNumeroQualquer = new Promise((resolve, reject) => {
+    const numero = parseInt(Math.random() * 100)
+    resolve(numero)
+})
+ */
+//tratando a resposta da promessa
+/* promessaDeUmNumeroQualquer
+    .then((value) => {
+        console.log(value)
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+    .finally(()=>{
+        console.log('Finalizou')
+    })
+ */
+//--------------------------------------------------------
+//simulando o assincronismo
+//criando uma promessa
+const promessaDeUmNumeroQualquer = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        const numero = parseInt(Math.random() * 100)
+        resolve(numero)
+    }, 1000)//mil milisegundos ou 1 segundo
+})
+
+//tratando a resposta da promessa
+ promessaDeUmNumeroQualquer
+    .then((value) => {
+        console.log(value)
+        return value +10
+    })
+    .then((value)=>{
+        console.log(value)
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+    .finally(()=>{
+        console.log('Finalizou')
+    })
+ 

1adaa88a8fe5e6cfbb85afbbe2f2b49b4cbaeb91
joaopedrogueds <joaopedroguedestorres@gmail.com>
2023-11-21 13:43:35 -0300

    adicionando especialidades ao portfolio

---
+++ Dio/Portfolio/assets/js/acordeon.js
@@ -0,0 +1,22 @@
+
+const acordeonTriggers = window.document.querySelectorAll('.acordeon .trigger') 
+
+
+ acordeonTriggers.forEach((trigger)=>{
+
+    trigger.addEventListener('click', (e)=>{
+        const acordeon = trigger.parentElement
+
+        const isOpen = acordeon.classList.contains('open')
+
+        if(isOpen) {
+            acordeon.classList.remove('open')
+        }else {
+            acordeon.classList.add('open')
+        }
+    })
+    
+}) 
+
+
+    

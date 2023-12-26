4b9e67c4345842fb68695ef18784d42f7528b033
joaopedrogueds <joaopedroguedestorres@gmail.com>
2023-12-26 11:03:48 -0300

    criei componente botao no react

--- Dio/React/modulo 6 react/aula3 Criando projeto React/dio/src/App.js
+++ Dio/React/modulo 6 react/aula3 Criando projeto React/dio/src/App.js
@@ -1,8 +1,13 @@
+ import Button from './components/Button' 
 
 function App() {
   return (
     <div className="App">
-      Ola React
+      <h1>Ola React</h1>
+       <Button title='Entrar'/> 
+       <Button title='Fechar'/>
+       <Button title='Abrir'/> 
+       <Button title='Salvar'/>  
     </div>
   );
 }
---
+++ Dio/React/modulo 6 react/aula3 Criando projeto React/dio/src/components/Button/index.js
@@ -0,0 +1,8 @@
+
+
+export default function Button(props) {
+    const { title } = props;
+
+  return <button>{title}</button>;
+  
+}

290c3cf32c633702eb0d11ca4a40e8316994cf14
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-01-01 11:42:42 -0300

    criei a funcao somar

--- Dio/React/modulo 7 Desafio Calculadora/desafio01-calculadora/src/App.js
+++ Dio/React/modulo 7 Desafio Calculadora/desafio01-calculadora/src/App.js
@@ -8,17 +8,35 @@ import { useState } from 'react'
 
 
 const App = () => {
-  const [currentNumber, setCurrentNumber] = useState(0);
+  const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber,setFirstNumber] = useState('0');
+  const [operation,setOperation] = useState('');
 
   const handleOnClear = () => {
-    setCurrentNumber(0)
+    setCurrentNumber('0');
+    setFirstNumber('0');
+    setOperation('');
   };
 
   const handleAddNumber = (num) => {
-    setCurrentNumber(prev => `${num} ${prev === 0 ? '' : prev} `);
+    setCurrentNumber(prev=>`${prev === '0' ? '' : prev}${num}`);
   }  
 
+  const handleSumNumbers = () => {
+    if(firstNumber==='0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+    } else {
+      /* console.log(firstNumber, Number(currentNumber)); */
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setFirstNumber('0');
+      setOperation('');
+      /* console.log(sum) */
+    }
+  }
 
+  
  
   return (
     <Container>
@@ -40,13 +58,13 @@ const App = () => {
           <Button label={"4"} onClick={() =>handleAddNumber('4')} />
           <Button label={"5"} onClick={() =>handleAddNumber('5')} />
           <Button label={"6"} onClick={() =>handleAddNumber('6')} />
-          <Button label={"+"} onClick={() =>handleAddNumber('+')} />
+          <Button label={"+"} onClick={handleSumNumbers} />
         </Row>
         <Row>
           <Button label={"1"} onClick={() =>handleAddNumber('1')} />
           <Button label={"2"} onClick={() =>handleAddNumber('2')} />
           <Button label={"3"} onClick={() =>handleAddNumber('3')} />
-          <Button label={"="} onClick={() =>handleAddNumber('=')} />
+          <Button label={"="} onClick={handleEquals} />
         </Row>
 
 

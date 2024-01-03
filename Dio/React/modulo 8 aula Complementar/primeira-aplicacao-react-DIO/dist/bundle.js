3409deb815b69d06a7987f3f7299e25633d66bf5
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-01-03 13:38:55 -0300

    analisei e estudei as configuracoes dos arquivos do react

---
+++ Dio/React/modulo 8 aula Complementar/primeira-aplicacao-react-DIO/dist/bundle.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function app() {
+  return /*#__PURE__*/_react["default"].createElement("h1", null, "Ola mundo!");
+}
---
+++ Dio/React/modulo 8 aula Complementar/primeira-aplicacao-react-DIO/src/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+function app(){
+    return <h1>Ola mundo!</h1>
+}

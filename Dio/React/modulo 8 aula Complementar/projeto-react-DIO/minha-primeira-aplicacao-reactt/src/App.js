4d7ad89c69c48e026082dd133abc5ae7adc61fe2
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-01-04 13:13:48 -0300

    terminei a aula complementar

--- Dio/React/modulo 8 aula Complementar/projeto-react-DIO/minha-primeira-aplicacao-reactt/src/App.js
+++ Dio/React/modulo 8 aula Complementar/projeto-react-DIO/minha-primeira-aplicacao-reactt/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [contador, updateContador] = useState(0);
+
+  function adicionarMaiUmNoContador(){
+    updateContador(contador+1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{contador}</h1>
+      <button onClick={adicionarMaiUmNoContador}>Contar</button>
     </div>
   );
 }

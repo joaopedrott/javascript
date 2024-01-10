80afaf94ae566228936b8319fd81d574384bcf50
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-01-10 12:37:07 -0300

    modulo 10 concluido

--- Dio/React/modulo 9 criando projeto react do zero/aula 1 babel/projeto/src/App.js
+++ Dio/React/modulo 9 criando projeto react do zero/aula 1 babel/projeto/src/App.js
@@ -1,7 +1,30 @@
+import React, {useState, useEffect} from "react";
 const App = () => {
+    const [usuario, setUsuario]=useState("")
+    const [usuarios, setUsuarios] = useState(["Pablo", "Jose", "Manoel"]);
+
+    const handleAddUser = () => {
+        setUsuarios ([...usuarios, usuario]);
+        setUsuario("");
+    }
+    
+
+
     return (
         <div className="App">
             <h1>Hello DIO!</h1>
+            <div>
+                <input
+                value={usuario}
+                onChange={(event)=> setUsuario(event.target.value)}
+                />
+                <button onClick={handleAddUser}>Adicionar</button>
+            </div>
+            <hr />
+            {usuarios.map ((item)=>(
+                <p>{item}</p>
+
+            ))}
         </div>
     );
 };

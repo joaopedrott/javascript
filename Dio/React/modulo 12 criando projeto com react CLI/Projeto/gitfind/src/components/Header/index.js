842d8ad7b0842594c55ca880000ca378ba2c6cd2
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-01-16 12:19:48 -0300

    fiz o header da pagina

---
+++ Dio/React/modulo 12 criando projeto com react CLI/Projeto/gitfind/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import './styles.css';
+const Header =()=> {
+  return (
+    <header>
+        
+        <h1>GitFind</h1>
+
+    </header>
+  )
+}
+
+export { Header };
--- Dio/React/modulo 12 criando projeto com react CLI/Projeto/gitfind/src/pages/Home/index.js
+++ Dio/React/modulo 12 criando projeto com react CLI/Projeto/gitfind/src/pages/Home/index.js
@@ -1,8 +1,8 @@
-
+import { Header } from '../../components/Header';
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   );
 }

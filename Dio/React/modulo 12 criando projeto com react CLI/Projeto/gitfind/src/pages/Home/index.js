82b277f6e5d3822382090dc0ba0a1ca47bf9a120
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-01-17 10:52:37 -0300

    formatei a imagem em css e adicionei a barra de busca com o botao

--- Dio/React/modulo 12 criando projeto com react CLI/Projeto/gitfind/src/pages/Home/index.js
+++ Dio/React/modulo 12 criando projeto com react CLI/Projeto/gitfind/src/pages/Home/index.js
@@ -1,8 +1,19 @@
 import { Header } from '../../components/Header';
+import background from '../../assets/background.png'
+import './styles.css'
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className="conteudo">
+        <img src={background} className="background" alt="background app"></img>
+        <div className="info">
+          <div>
+            <input name="usuario" placeholder="@username" />
+            <button>Buscar</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

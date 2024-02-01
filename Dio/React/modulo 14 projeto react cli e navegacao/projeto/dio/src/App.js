41bceea85932756126dd9defb1cfcb0bdc22b5a5
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-02-01 13:55:27 -0300

    modulo 15 hooks

--- Dio/React/modulo 14 projeto react cli e navegacao/projeto/dio/src/App.js
+++ Dio/React/modulo 14 projeto react cli e navegacao/projeto/dio/src/App.js
@@ -7,6 +7,7 @@ import {
 import { Home } from './pages/home';
 import { Login } from './pages/login';
 import { Feed } from './pages/feed';
+import {Teste} from './pages/index';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route  path="/login" element={<Login/>}/> 
         <Route  path="/feed" element={<Feed/>}/> 
+        <Route  path="/teste" element={<Teste/>}/> 
       </Routes>
     </Router>
   );
---
+++ Dio/React/modulo 14 projeto react cli e navegacao/projeto/dio/src/pages/index.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import { Button } from '../components/Button';
+
+
+const Teste=()=> {
+
+ /*
+ modulo 16 useState koock 
+
+ //Exemplo de uso de useState para guardar o nome digitado e em seguida o nome eh imprimido no console
+    const [nomes, setNomes] = useState([]);
+    const [nome, setNome] = useState('');
+  
+    function handleInputChange(event) {
+      setNome(event.target.value);
+    }
+  
+    function handleAddButtonClick() {
+      const newNomes = [...nomes];
+      newNomes.push(nome);
+      setNomes(newNomes);
+      setNome('');
+    }
+  
+    function handlePrintButtonClick() {
+      console.log(nomes);
+    }
+  
+    return (
+      <div>
+        <input value={nome} onChange={handleInputChange}/>
+        <button onClick={handleAddButtonClick}>Adicionar</button>
+        <button onClick={handlePrintButtonClick}>Imprimir</button>
+      </div>
+    ); */
+//--------------------------------------------------------------------------------------------------------
+    /* //modulo 16 useState koock 
+     const [name, setName] = useState('Pablo');
+
+
+    const handleChangeName =()=>{
+        setName('Jose')
+    }
+    return (
+    <div>
+        <p>
+            {name}
+        </p>
+        <button onClick={handleChangeName}>Alterar</button>
+    </div>
+  ) */
+  
+} 
+
+export { Teste }

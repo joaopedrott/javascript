d956ef95e63313c508e967235479343c37b925cd
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-02-05 12:58:43 -0300

    finalizei estudo de hooks modulo 16

--- Dio/React/modulo 14 projeto react cli e navegacao/projeto/dio/src/pages/index.jsx
+++ Dio/React/modulo 14 projeto react cli e navegacao/projeto/dio/src/pages/index.jsx
@@ -1,12 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo, useCallback } from 'react'
 import { Button } from '../components/Button';
 
 
 const Teste=()=> {
 
  /*
- modulo 16 useState koock 
-
+ //modulo 16  
+//aula2 useState kook
+//guardar estado de um componente
  //Exemplo de uso de useState para guardar o nome digitado e em seguida o nome eh imprimido no console
     const [nomes, setNomes] = useState([]);
     const [nome, setNome] = useState('');
@@ -34,7 +35,10 @@ const Teste=()=> {
       </div>
     ); */
 //--------------------------------------------------------------------------------------------------------
-    /* //modulo 16 useState koock 
+//modulo 16  
+//aula2 useState hook
+    /* 
+    
      const [name, setName] = useState('Pablo');
 
 
@@ -49,7 +53,68 @@ const Teste=()=> {
         <button onClick={handleChangeName}>Alterar</button>
     </div>
   ) */
-  
+  //--------------------------------------------------------------------------------------------------------
+  //modulo 16 
+  //aula 4 useMemo hook
+  /* const [age, setAge] = useState(26);
+
+
+  const handleChangeName =()=>{
+    setAge(prev => prev === 26? 32:26)
+  }
+
+  const calculo =useMemo(()=>{
+    console.log('calculou',age)
+    return 10 * age;
+  },[age]);
+
+  console.log('renderizou', calculo);
+  return (
+  <div>
+      <p>
+          {age}
+      </p>
+      <button onClick={handleChangeName}>Alterar</button>
+  </div>
+) */
+//--------------------------------------------------------------------------------------------------------
+//modulo 16 
+  //aula 5 useCallback hook
+
+  const [age, setAge] = useState(26);
+  const [name, setName] = useState('Pablo');
+
+  const handleChangeName =useCallback(()=>{
+    console.log('Alterou nome')
+    setName(prev => prev === 'Pablo'? 'Jose':'Pablo')
+  },[])
+
+  const handleChangeAge = useCallback(()=>{
+    const newAge = 10* age;
+    console.log('age atual ',age,'age final ', newAge)
+    setAge(prev => prev === 26? 32:26)
+  },[age])
+
+ 
+
+  return (
+  <div>
+      <p>
+          Idade: {age}
+      </p>
+      <br />
+      <p>
+        Nome: {name}
+      </p>
+      <br />
+      <button onClick={handleChangeName}>Alterar Nome</button>
+      <br />
+      <button onClick={handleChangeAge}>Alterar Idade</button>
+  </div>
+)
+
+
 } 
 
 export { Teste }
+

7054bd35d9b76b2ab570260da80ebce875b552c4
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-05-16 13:57:41 -0300

    Update script.js

--- DEV Sem Limites/Aulas/DevBot/script.js
+++ DEV Sem Limites/Aulas/DevBot/script.js
@@ -11,7 +11,25 @@ function hendleChatOnKeyDown (event) { //funcao para enviar mensagem apertando e
 }
 
 async function createBotReply (content) {//funcao do chat bot do chatGPT
+    const API_URL = 'https://api.openai.com/v1/chat/completions'
+    const API_KEY = 'Sua API KEY AQUI!'
 
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json',
+            'Authorization' : `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: [{
+                role: 'user',
+                content
+            }]
+        })
+    })
+    const data = await response.json()
+    return data.choices[0].message.content
 }
 
 function createChatMessage (message, type){ //cria mensagem seja do usuario ou do bot

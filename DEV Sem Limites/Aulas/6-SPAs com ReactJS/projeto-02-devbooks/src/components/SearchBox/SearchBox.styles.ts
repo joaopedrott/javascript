b69e0e885c5f2f051f7124a2ba478b04f3125fd5
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-06-27 17:13:13 -0300

    criei o componente searchbox para a primeira pagina

---
+++ DEV Sem Limites/Aulas/6-SPAs com ReactJS/projeto-02-devbooks/src/components/SearchBox/SearchBox.styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div`  
+    display: flex;
+    align-items: center;
+    border: 2px solid #d9d9d9;
+    border-radius: 10px;
+    padding: 0 10px;
+    flex: 1;
+
+    svg {
+        height: 20px;
+        width: 20px;
+    }
+
+    input {
+        width: 100%;
+        height: 46px;
+        border: 0;
+        margin-left: 12px;
+        outline: none;
+    }
+  `
+
--- DEV Sem Limites/Aulas/6-SPAs com ReactJS/projeto-02-devbooks/src/pages/Search/Search.tsx
+++ DEV Sem Limites/Aulas/6-SPAs com ReactJS/projeto-02-devbooks/src/pages/Search/Search.tsx
@@ -1,5 +1,10 @@
+import { SearchBox } from '../../components/SearchBox'
+
 export function Search() {
     return (
+        <>
         <h1>Busque seus livros favoritos!</h1>
+        <SearchBox />
+        </>
     )
 }

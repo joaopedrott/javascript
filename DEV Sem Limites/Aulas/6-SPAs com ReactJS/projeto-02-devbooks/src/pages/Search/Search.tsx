e2431b6650a9bbcd308d8bc8aba692520c7796ce
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-07-01 18:44:05 -0300

    estilizei o meu projeto usando o styled component e fiz o botao buscar funcionar

--- DEV Sem Limites/Aulas/6-SPAs com ReactJS/projeto-02-devbooks/src/pages/Search/Search.tsx
+++ DEV Sem Limites/Aulas/6-SPAs com ReactJS/projeto-02-devbooks/src/pages/Search/Search.tsx
@@ -1,10 +1,28 @@
+import { Container, SearchContainer, SearchButton } from './Search.styles'
+
 import { SearchBox } from '../../components/SearchBox'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export function Search() {
+    const [search, setSearch] =useState('')
+    const navigate = useNavigate()
+
+    const handleSearch = () => {
+        if (search) {
+            //navegar para a pagina de resultados
+            navigate(`/books?q=${search}`)
+        }
+    }
+
     return (
-        <>
-        <h1>Busque seus livros favoritos!</h1>
-        <SearchBox />
-        </>
+        <Container>
+            <h1>Busque seus livros favoritos!</h1>
+            <SearchContainer>
+                <SearchBox value={search} onChange={(e)=> setSearch(e.target.value)} />
+                <SearchButton onClick={handleSearch}>Buscar</SearchButton>
+            </SearchContainer>
+            
+        </Container>
     )
 }

7666e27d2ad4f0454110e221d9ca0dad1cd9422c
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-07-02 14:30:53 -0300

    Anotacoes sobre useEffect

--- DEV Sem Limites/Aulas/6-SPAs com ReactJS/projeto-02-devbooks/src/pages/Books/Books.tsx
+++ DEV Sem Limites/Aulas/6-SPAs com ReactJS/projeto-02-devbooks/src/pages/Books/Books.tsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from 'react'
+//entendendo o useEffect
 export function Books () {
+    const [count, setCount] = useState(0)
+
+    /* useEffect(()=>{
+        // imprimi a palavra Efeito a cada 1 segundo
+        const interval = setInterval(()=> { 
+            console.log('Efeito')
+        }, 1000)
+
+        //se caso mudarmos de pagina o use effect para gracas a esse return
+        return () => {
+            clearInterval(interval)
+        }
+
+    },[])//com a chaves "[]" (array de dependencia) vazia, podemos fazer com que o useEffect so execute uma vez e bem na hora que a pagina abrir. */
+
     return (
-        <h1>Resultados da busca</h1>
+        <>
+        <span>{count}</span>
+        <button onClick={()=> setCount(count+1)}>Increment</button>
+        </>
     )
 }

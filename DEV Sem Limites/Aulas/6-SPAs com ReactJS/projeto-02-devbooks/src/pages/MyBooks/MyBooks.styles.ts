923184aa1b5cd35af53673647baee4e7572626bb
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-09-11 12:55:16 -0300

    43 criei o estilo do card dos livros "Estou lendo"

--- DEV Sem Limites/Aulas/6-SPAs com ReactJS/projeto-02-devbooks/src/pages/MyBooks/MyBooks.styles.ts
+++ DEV Sem Limites/Aulas/6-SPAs com ReactJS/projeto-02-devbooks/src/pages/MyBooks/MyBooks.styles.ts
@@ -7,27 +7,103 @@ export const Container = styled.div`
 `
 
 export const BookContainer = styled.div`
-${({theme})=>css`
-    display: flex;
-    gap: ${theme.spacings.small};
-    margin-top: 1.6rem;
-    margin-bottom: 6.4rem;
-    flex-wrap: wrap;
-`}
-
+    ${({theme})=>css`
+        display: flex;
+        gap: ${theme.spacings.small};
+        margin-top: 1.6rem;
+        margin-bottom: 6.4rem;
+        flex-wrap: wrap;
+    `}
 `
+
+export const ReadingList = styled.ul`
+
+    ${({theme})=>css`
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(30rem, 1fr));
+        gap: ${theme.spacings.small};
+        margin-top: ${theme.spacings.xsmall};
+        margin-bottom: 6.4rem;
+        list-style: none;
+    `}
+`
+
+export const ReadingCard = styled.div`
+    ${({theme})=>css`
+        display: flex;
+        border: 0.1rem solid ${theme.colors.gray};
+        border-radius: ${theme.border.radius.default};
+        padding: ${theme.spacings.xsmall};
+        width: 100%;
+        background-color: ${theme.colors.white};
+
+        h2 {
+            font-size: ${theme.font.sizes.large};
+            ${clampText(2)}
+        }
+
+        h3 {
+            font-size: ${theme.font.sizes.small};
+            margin-top: 0.4rem;
+            font-weight: normal;
+        }
+    `}
+`
+
+export const Details = styled.div`
+    ${({theme})=> css`
+        flex: 1;
+        margin-left: ${theme.spacings.xsmall};
+    `}
+`
+export const ProgressBarContainer = styled.div`
+    ${({theme})=>css`
+        display: flex;
+        align-items: center;
+
+        span {
+            font-size: ${theme.font.sizes.small};
+            margin-left: ${theme.spacings.xxsmall};
+        }
+    `}
+`
+interface ProgressBarProps {
+    progress:number;
+}
+
+export const ProgressBar = styled.div<ProgressBarProps>`
+    ${({theme, progress})=>css`
+        margin: ${theme.spacings.xsmall};
+        height: 0.6rem;
+        background-color: ${theme.colors.gray};
+        border-radius: ${theme.border.radius.small};
+        flex: 1;
+        position: relative;
+
+        &::after {
+            content: '';
+            height: 0.6rem;
+            width: ${progress}%;
+            background-color: ${theme.colors.primary};
+            position: absolute;
+            border-radius: ${theme.border.radius.small};
+        }
+    `}
+`
+export const PageCountText = styled.span`
+    ${({theme})=>css`
+        display: block;
+        margin-bottom: ${theme.spacings.xsmall};
+        font-size: ${theme.font.sizes.xsmall};
+        
+    `}
+`
+
 
 export const Book = styled.div`
 ${({theme})=>css`
 
 width: 12rem;
-    img {
-        height: 16rem;
-        width: 12rem;
-        object-fit: fill;
-        border: ${theme.border.radius.small};
-
-    }
 
     h2 {
         font-size:  ${theme.font.sizes.medium};
@@ -43,4 +119,16 @@ width: 12rem;
 
     }
 `}
+`
+
+export const Thumbnail = styled.img`
+    ${({theme})=>css`
+        height: 16rem;
+        width: 12rem;
+        object-fit: fill;
+        border: ${theme.border.radius.small};
+        box-shadow: 0 1px 2px 0 rgba(60,64,67,0.3), 0 1px 3px 1px rgba(60, 64, 67, 0.15);
+    
+    `}
+
 `
--- DEV Sem Limites/Aulas/6-SPAs com ReactJS/projeto-02-devbooks/src/pages/MyBooks/MyBooks.tsx
+++ DEV Sem Limites/Aulas/6-SPAs com ReactJS/projeto-02-devbooks/src/pages/MyBooks/MyBooks.tsx
@@ -1,7 +1,8 @@
+import { Button } from "../../components/Button";
 import { useMyBooksQuery } from "../../hooks/useMyBooksQuery";
 import { MainLayout } from "../../Layouts/MainLayout";
 import { generateThumbnailSrc } from "../../utils/generateThumbnailSrc";
-import { Book, BookContainer, Container } from "./MyBooks.styles";
+import { Book, BookContainer, Container, Details, PageCountText, ProgressBar, ProgressBarContainer, ReadingCard, ReadingList, Thumbnail } from "./MyBooks.styles";
 
 export function MyBooks () {
     const { data, isLoading } =useMyBooksQuery()
@@ -12,15 +13,35 @@ export function MyBooks () {
                 <Container>
                     <div>
                         <h1>Lendo</h1>
-                        <BookContainer>
+                        <ReadingList>
                             {data.isReading.map((item) => (
-                                <Book key={item.bookId}>
-                                    <img src={generateThumbnailSrc({bookId: item.bookId})} alt={item.book.volumeInfo.title} />
+                                <li key={item.bookId}>
+                                    <ReadingCard >
+                                        <Thumbnail src={generateThumbnailSrc({bookId: item.bookId})} alt={item.book.volumeInfo.title} />
 
-                                    <h2>{item.book.volumeInfo.title}</h2>
-                                    {item.book.volumeInfo.authors &&(<h3>{item.book.volumeInfo.authors[0]}</h3>) }
-                                </Book>))}
-                        </BookContainer>
+                                        <Details>
+                                            <h2>{item.book.volumeInfo.title}</h2>
+                                            {item.book.volumeInfo.authors &&(<h3>{item.book.volumeInfo.authors[0]}</h3>) }
+
+                                            <ProgressBarContainer>
+                                                <ProgressBar progress={30}/>
+                                                <span>30%</span>
+                                            </ProgressBarContainer>
+
+                                            <PageCountText>
+                                                Faltam 200 pag. para terminar.
+                                            </PageCountText>
+                                            
+                                            <Button variant="outlined" color="secondary" size="small"
+                                            fullWidh>Atualizar Leitura 
+                                            </Button>
+                                        </Details>
+                                    </ReadingCard>
+
+                                </li>
+                            
+                            ))}
+                        </ReadingList>
                     </div>
 
                     <div>
@@ -28,7 +49,7 @@ export function MyBooks () {
                         <BookContainer>
                             {data.wantsToRead.map((item) => (
                                 <Book key={item.bookId}>
-                                    <img src={generateThumbnailSrc({bookId: item.bookId})} alt={item.book.volumeInfo.title} />
+                                    <Thumbnail src={generateThumbnailSrc({bookId: item.bookId})} alt={item.book.volumeInfo.title} />
 
                                     <h2>{item.book.volumeInfo.title}</h2>
                                     {item.book.volumeInfo.authors &&(<h3>{item.book.volumeInfo.authors[0]}</h3>) }
@@ -41,7 +62,7 @@ export function MyBooks () {
                         <BookContainer>
                             {data.read.map((item) => (
                                 <Book key={item.bookId}>
-                                    <img src={generateThumbnailSrc({bookId: item.bookId})} alt={item.book.volumeInfo.title} />
+                                    <Thumbnail src={generateThumbnailSrc({bookId: item.bookId})} alt={item.book.volumeInfo.title} />
 
                                     <h2>{item.book.volumeInfo.title}</h2>
                                     {item.book.volumeInfo.authors &&(<h3>{item.book.volumeInfo.authors[0]}</h3>) }

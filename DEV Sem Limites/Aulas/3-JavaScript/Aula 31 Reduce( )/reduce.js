b6f8de6baa6fe9274f47e76a0752c3d62167307d
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-09-27 14:41:15 -0300

    Update reduce.js

--- DEV Sem Limites/Aulas/3-JavaScript/Aula 31 Reduce( )/reduce.js
+++ DEV Sem Limites/Aulas/3-JavaScript/Aula 31 Reduce( )/reduce.js
@@ -6,17 +6,16 @@ pega os elementos da array e reduz a um unico valor
 
 */
 
-const funcionarios  = [
-    { nome : 'Douglas', salario: 10000},
-    { nome : 'Ana', salario: 12000},
-    { nome : 'Maria', salario: 15000}
-]
+const funcionarios = [
+  { nome: "Douglas", salario: 10000 },
+  { nome: "Ana", salario: 12000 },
+  { nome: "Maria", salario: 15000 },
+];
 
-
-const salarioTotal = funcionarios.reduce((total, funcionario)=>{
-    return total + funcionario.salario
-}, 0)
-console.log(salarioTotal)
+const salarioTotal = funcionarios.reduce((total, funcionario) => {
+  return total + funcionario.salario;
+}, 0);
+console.log(salarioTotal);
 
 //reduce pode receber ate 3 parametros total, funcionario, index e o zero ( 0 )
 /*
@@ -27,4 +26,24 @@ console.log(salarioTotal)
 *index
 
 *0 (valor de inicializacao) para nesse caso comecar a contagem do acumulo a partir do zero ( 0 )
+*/
+
+/*
+Da para fazer uma array usando reduce tambem
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+// Usar reduce para filtrar números pares e dobrar os valores
+const result = numbers.reduce((acc, n) => {
+  if (n % 2 === 0) {
+    acc.push(n * 2);
+  }
+  return acc;
+}, []);
+console.log(result); // [4, 8, 12]
+
+
+
+
+
 */

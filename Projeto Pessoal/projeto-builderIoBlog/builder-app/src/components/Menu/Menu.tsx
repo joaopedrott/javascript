f520db9a39c2ff0602a11fa5b8592973820fb12e
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-10-02 15:08:39 -0300

    criei projeto teste do builder.io

---
+++ Projeto Pessoal/projeto-builderIoBlog/builder-app/src/components/Menu/Menu.tsx
@@ -0,0 +1,18 @@
+// src/components/Menu.tsx
+"use client";
+import React from 'react';
+
+
+export function Menu() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Menu;

721c5bb16fc03cd431b25b943db820f799ae3c33
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-10-04 18:18:32 -0300

    configurando api da steam

---
+++ Projeto Pessoal/projeto-BlogDeNoticias/projeto-BlogNoticias/src/services/steamService.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const apiKey = 'SUA_CHAVE_API';
+
+export const getSteamUserData = async (steamId) => {
+  try {
+    const response = await axios.get(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${apiKey}&steamids=${steamId}`);
+    return response.data.response.players[0];
+  } catch (error) {
+    console.error('Erro ao buscar dados do usuário Steam:', error);
+    throw error;
+  }
+};

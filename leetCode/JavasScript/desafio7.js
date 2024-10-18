b17c0375bcd70a6c8a0ea2cc580a959b7acb1793
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-10-18 18:12:36 -0300

    desafio 7

---
+++ leetCode/JavasScript/desafio7.js
@@ -0,0 +1,22 @@
+var reduce = function(nums, fn, init) {
+    let accum = init
+    nums.forEach((curr)=>{
+         
+        accum=fn(accum,curr)
+        
+    })
+    
+    return accum
+};
+
+
+
+let nums = [1,2,3,4];
+
+const fn = function sum(accum, curr) {
+    return accum + curr; 
+}
+
+let init= 0
+
+console.log(reduce(nums,fn,init))

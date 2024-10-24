5ee140430e71e8402d9ced8b1b91b8ca0e9fb48d
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-10-24 13:07:13 -0300

    desafio11

--- leetCode/JavasScript/desafio11.js
+++ leetCode/JavasScript/desafio11.js
@@ -4,7 +4,8 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    
+    const [value1, value2] = await Promise.all([promise1, promise2]);
+    return value1 + value2;
 };
 
 /**

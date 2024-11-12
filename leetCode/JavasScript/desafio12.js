045d2f63d01c847100912d8463e088712eede5e8
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-11-12 15:21:02 -0300

    Update desafio12.js

--- leetCode/JavasScript/desafio12.js
+++ leetCode/JavasScript/desafio12.js
@@ -14,6 +14,16 @@ async function sleep(millis) {// recebe milisegundos
 
 
  
-  let t = Date.now()
+  let t = Date.now()// tempo inicial
   sleep(100).then(() => console.log(Date.now() - t)) // 100
+
+//obs: o Date.now() retorna o tempo atual em milisegundos desde a época (1 de janeiro de 1970)
+// t representa o tempo inicial e quando chamo date.now() ele retorna o tempo atualizado entao so eh subtrair para encontraro tempo passado desde o inicio ate o fim
+
+/*
+neste caso pega o tempo antes da executao de sleep
+quando sleed termina o tempo estipulado para setTimeout, eh pego o tempo novamente e subtrai do tempo pego inicialmente para ter o tempo passado desde o inicio ate o fim do codigo
+
+
+*/
  

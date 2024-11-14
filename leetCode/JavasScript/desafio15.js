d14f0982960c53651f73a4a9878a23a56c434e7f
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-11-14 10:48:13 -0300

    objeto vazio

---
+++ leetCode/JavasScript/desafio15.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    if(Object.keys(obj).length===0) {
+        return true
+    } else {
+        return false
+    }
+    //return Object.keys(obj).length === 0;
+};

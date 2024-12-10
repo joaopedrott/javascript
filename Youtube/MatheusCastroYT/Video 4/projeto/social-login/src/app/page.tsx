d37725f5d26c348e70c7fa02722d44716fbca7b4
joaopedrogueds <joaopedroguedestorres@gmail.com>
2024-12-10 13:02:12 -0300

    modificacoes

--- Youtube/MatheusCastroYT/Video 4/projeto/social-login/src/app/page.tsx
+++ Youtube/MatheusCastroYT/Video 4/projeto/social-login/src/app/page.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function Login() {
   return (
-    <h1>
-      Login Social
-    </h1>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Card>
+        <CardHeader>
+          <CardTitle>Login Social</CardTitle>
+          <CardDescription>Faca seu login com sua conta Google</CardDescription>
+
+          <CardContent>
+            <form>
+              <Button>Login com Google</Button>
+            </form>
+          </CardContent>
+        </CardHeader>
+      </Card>
+    </div>
   );
 }

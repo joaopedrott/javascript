efd41502b6f729da12dd979f56173ab02d8e6375
joaopedrogueds <joaopedroguedestorres@gmail.com>
2025-01-22 10:40:29 -0300

    Update thisLexico.js

--- DEV Sem Limites/Aulas/3-JavaScript/Aula 25 This lexico/thisLexico.js
+++ DEV Sem Limites/Aulas/3-JavaScript/Aula 25 This lexico/thisLexico.js
@@ -4,3 +4,14 @@ This (lexico)
     Quando o This eh feito dentro de uma arrow function ele sempre vai ser definido a partir do escopo lexido na qual a funcao foi definida e nunca mudara.
 
 */
+
+
+/*
+
+o this dentro de uma funcao comum, muda de valor de acordo com o contexto em que foi chamado.
+
+Ja dentro de uma arrow function, o this nao muda de valor, ele sempre vai ser definido a partir do escopo lexico na qual a funcao foi definida e NUNCA mudara.
+
+
+
+*/

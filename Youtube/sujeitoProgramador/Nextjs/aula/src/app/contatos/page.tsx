081385350b75a08073fb55264f8b70949962ced9
joaopedrogueds <joaopedroguedestorres@gmail.com>
2025-01-23 12:23:32 -0300

    teste do nextjs

---
+++ Youtube/sujeitoProgramador/Nextjs/aula/src/app/contatos/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Contatos() {
+  return (
+    <div>
+        <h1>Pagina Contatos</h1>
+    </div>
+  )
+}
---
+++ Youtube/sujeitoProgramador/Nextjs/aula/src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Dashboard() {
+  return (
+    <div>
+        <h1>Pagina Dashboard</h1>
+    </div>
+  )
+}

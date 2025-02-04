4f8c9b7ef67e18f859e0ef167aaafa5fcf46bf6b
joaopedrogueds <joaopedroguedestorres@gmail.com>
2025-02-04 10:51:46 -0300

    Route handlers

---
+++ Youtube/sujeitoProgramador/Nextjs/aula/src/app/api/info/route.ts
@@ -0,0 +1,11 @@
+
+
+export async function GET() {
+    return Response.json({
+        
+            name: "Sujeito Programador",
+            intagram: "@sujeitoprogramador",
+            youtube: 'Sujeito Programador'
+        
+    })
+}

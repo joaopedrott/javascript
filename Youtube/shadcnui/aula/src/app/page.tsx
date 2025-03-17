9da71e8fe1dcb0e41b1ef7c1d7b54491d1aa9cd7
joaopedrogueds <joaopedroguedestorres@gmail.com>
2025-03-17 11:34:22 -0300

    criei projeto next e instalei shadcnui

---
+++ Youtube/shadcnui/aula/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="text-red-500 text-2xl font-black">
+      <h1>Projeto Deshboard</h1>
+    </main>
+  );
+}

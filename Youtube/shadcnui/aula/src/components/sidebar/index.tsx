edc6acab2f68cc03220d297228bef6b80484100d
joaopedrogueds <joaopedroguedestorres@gmail.com>
2025-03-19 11:32:49 -0300

    criei o menu para desktop

--- Youtube/shadcnui/aula/src/components/sidebar/index.tsx
+++ Youtube/shadcnui/aula/src/components/sidebar/index.tsx
@@ -1,12 +1,112 @@
 import { Sheet, SheetTrigger,SheetContent } from '@/components/ui/sheet';
 import { Button } from '../ui/button';
 import Link from 'next/link';
-import { Home, Package, PanelBottom, Settings2, ShoppingBag, Users } from 'lucide-react';
+import { Home, LogOut, Package, PanelBottom, Settings2, ShoppingBag, Users } from 'lucide-react';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/tooltip';
 
 export function Sidebar(){
     return(
         <div className="flex w-full flex-col bg-muted/40">
+            {/*!--- menu desktop --- */}
+            <aside className='fixed inset-y-0 left-0 z-10 hidden w-14 border-r bg-background sm:flex flex-col'>
+                <nav className='flex flex-col items-center gap-4 px-2 py-5'>
+                <TooltipProvider>
+                    <Link 
+                    href='#'
+                    className='flex h-9 w-9 shrink-0 items-center justify-center bg-primary text-primary-foreground rounded-full'
+                    >
+                        <Package className='h-4 w-4'/>
+                        <span className='sr-only'>Dashboard Avatar</span>
+                    </Link>
 
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link 
+                            href='#'
+                            className='flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground'
+                            >
+                                <Home className='h-5 w-5'/>
+                                <span className='sr-only'>Inicio</span>
+                            </Link>
+                        </TooltipTrigger>
+                        <TooltipContent side='right'>Inicio</TooltipContent>
+                    </Tooltip>
+
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link 
+                            href='#'
+                            className='flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground'
+                            >
+                                <ShoppingBag className='h-5 w-5'/>
+                                <span className='sr-only'>Pedidos</span>
+                            </Link>
+                        </TooltipTrigger>
+                        <TooltipContent side='right'>Pedidos</TooltipContent>
+                    </Tooltip>
+
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link 
+                            href='#'
+                            className='flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground'
+                            >
+                                <Package className='h-5 w-5'/>
+                                <span className='sr-only'>Produtos</span>
+                            </Link>
+                        </TooltipTrigger>
+                        <TooltipContent side='right'>Produtos</TooltipContent>
+                    </Tooltip>
+
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link 
+                            href='#'
+                            className='flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground'
+                            >
+                                <Users className='h-5 w-5'/>
+                                <span className='sr-only'>Clientes</span>
+                            </Link>
+                        </TooltipTrigger>
+                        <TooltipContent side='right'>Clientes</TooltipContent>
+                    </Tooltip>
+
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link 
+                            href='#'
+                            className='flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground'
+                            >
+                                <Settings2 className='h-5 w-5'/>
+                                <span className='sr-only'>Configuracoes</span>
+                            </Link>
+                        </TooltipTrigger>
+                        <TooltipContent side='right'>Configuracoes</TooltipContent>
+                    </Tooltip>
+
+                </TooltipProvider>
+                </nav>
+
+                <nav className='mt-auto flex flex-col items-center gap-4 px-2 py-5'>
+                <TooltipProvider>
+                <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link 
+                            href='#'
+                            className='flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground'
+                            >
+                                <LogOut className='h-5 w-5 text-red-500'/>
+                                <span className='sr-only'>Sair</span>
+                            </Link>
+                        </TooltipTrigger>
+                        <TooltipContent side='right'>Sair</TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+                </nav>
+            </aside>
+            {/*!--- /menu desktop --- */}
+
+            {/*!--- menu mobile --- */}
             <div className="sm:hidden flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
                 <header className='sticky top-0 z-30 flex h-14 items-center px-4 border-b bg-background gap-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6'>
                     <Sheet>
@@ -78,6 +178,7 @@ export function Sidebar(){
                     <h2>Menu</h2>
                 </header>
             </div>
+            {/*!--- /menu mobile --- */}
         </div>
     )
 }

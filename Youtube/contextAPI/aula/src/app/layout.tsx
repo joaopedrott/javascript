5a5b634b6c793f155f7fbd341524a60285273fff
joaopedrogueds <joaopedroguedestorres@gmail.com>
2025-03-25 12:03:05 -0300

    criei  e fiz o contexto ser usado

--- Youtube/contextAPI/aula/src/app/layout.tsx
+++ Youtube/contextAPI/aula/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "@/contexts/auth";
+import { use } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,8 +28,11 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      > 
+      <AuthProvider>
         {children}
+      </AuthProvider>
+        
       </body>
     </html>
   );
---
+++ Youtube/contextAPI/aula/src/contexts/auth/index.tsx
@@ -0,0 +1,49 @@
+'use client'
+import { createContext, useState } from "react";  
+
+export type UseProps = {
+    name: string;
+    token: string;
+}
+
+type AuthContextProps = {
+    user: UseProps | null;
+    login: (user: UseProps) => void;
+    logout: () => void;
+}
+
+//contexto criado
+const AuthContext = createContext<AuthContextProps>({} as AuthContextProps);
+
+//criacao do provider
+const AuthProvider = ({ children }:{children: React.ReactNode}) => {
+    const [user, setUser] = useState<UseProps | null>({
+        name: 'Lucas',
+        token: '123'
+    });
+
+    const login = (user: UseProps) => {
+        setUser(user);
+
+        console.log(user);
+    }
+
+    const logout = () => {
+        console.log('logout');
+        setUser(null);
+    }
+
+    return (
+        <AuthContext.Provider value={{
+            user,
+            login,
+            logout
+        }}>
+            {children}
+        </AuthContext.Provider>
+
+    )
+
+}
+
+export { AuthContext, AuthProvider };

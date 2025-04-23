8694996dd316c744b3ec83323523c59159d57769
joaopedrogueds <joaopedroguedestorres@gmail.com>
2025-04-23 11:50:56 -0300

    corrigi o passo 9

--- DEV Sem Limites/Aulas/8-Dominando NextJS/task-master/src/modules/teams/route.ts
+++ DEV Sem Limites/Aulas/8-Dominando NextJS/task-master/src/modules/teams/route.ts
@@ -1,11 +1,10 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
-
-app.get('/', (c) => {
+const app = new Hono().get('/', (c) => {
   return c.json({
     message: 'Hello from teams route!',
   })
 })
+
 
 export default app

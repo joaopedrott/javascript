d437014cb61732a5165d303bc74f397b0e7b9348
joaopedrogueds <joaopedroguedestorres@gmail.com>
2026-03-20 20:53:12 -0300

    geracao de codigo feita

--- DevClub/aulas/PROJETO-CSS-2026/scripts.js
+++ DevClub/aulas/PROJETO-CSS-2026/scripts.js
@@ -1,3 +1,37 @@
 let botao = document.querySelector(".botao-gerar");
+let chave = "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"
+let endereco = "https://api.groq.com/openai/v1/chat/completions"
 
-console.log(botao);
+async function gerarCodigo() {
+    let textoUsuario = document.querySelector(".caixa-texto").value;
+    let blocoCodigo = document.querySelector(".bloco-codigo");
+    let resultadoCodigo = document.querySelector(".resultado-codigo");
+
+    let resposta = await fetch(endereco, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${chave}`
+        },
+        body: JSON.stringify({
+            model: "llama-3.3-70b-versatile",
+            messages: [{
+                role: "system",
+                content: "Você é um gerador de código HTML e CSS. Responda SOMENTE com código puro. NUNCA use crases, markdown ou explicações. Formato: primeiro <style> com o CSS, depois o HTML. Siga EXATAMENTE o que o usuário pedir. Se pedir algo quicando, use translateY no @keyframes. Se pedir algo girando, use rotate."
+            },
+            {
+                role: "user",
+                content: textoUsuario
+            }
+            ]
+        })
+    })
+
+    let dados = await resposta.json();
+    let resultado = dados.choices[0].message.content;
+    
+    blocoCodigo.textContent = resultado;
+    resultadoCodigo.srcdoc = resultado
+}
+
+botao.addEventListener("click",gerarCodigo)
